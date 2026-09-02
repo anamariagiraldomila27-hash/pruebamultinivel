b06d1ce2de67bce6122acdeb90a258e3d4344934
Ana Giraldo <anamariagiraldomila27@gmail.com>
2026-09-02 00:01:00 -0500

    [modelo][semana-1] crea clase Personaje

---
+++ src/main/java/com/udistrital/rpgmanager/Mavenproject2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.udistrital.rpgmanager;
+
+/**
+ *
+ * @author AnaGiraldo
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
